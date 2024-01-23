887922e5a31d06c03fe6ec34189b7438a554d0f2
ReAaNdxD <andrearositani17@gmail.com>
2024-01-23 16:43:29 +0100

    ciao

--- ProgettoClaudioAndrea/src/main/java/it/unirc/pwm/actions/privato/articolo/ArticoloJSON.java
+++ ProgettoClaudioAndrea/src/main/java/it/unirc/pwm/actions/privato/articolo/ArticoloJSON.java
@@ -1,7 +1,6 @@
 package it.unirc.pwm.actions.privato.articolo;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -9,23 +8,24 @@ import it.unirc.pwm.ht.Articolo;
 import it.unirc.pwm.ht.dao.ArticoloDAO;
 import it.unirc.pwm.ht.dao.ArticoloDAOFactory;
 
-public class SearchJSON extends ActionSupport {
+public class ArticoloJSON extends ActionSupport {
 	private static final long serialVersionUID = 1L;
-	private ArrayList<Articolo> articoloList;
+	private ArrayList<Articolo> result;
 
 	public ArrayList<Articolo> getArticoloList() {
-		return articoloList;
+		return result;
 	}
 
-	public void setArticoloList(ArrayList<Articolo> articoloList) {
-		this.articoloList = articoloList;
+	public void setArticoloList(ArrayList<Articolo> result) {
+		this.result = result;
 	}
 
 	@Override
 	public String execute() {
+		System.out.println("SONO ArticoloJSON");
 		ArticoloDAO articoloDAO = ArticoloDAOFactory.getDAO();
-		articoloList = articoloDAO.visualizzaArticoliJSON();
-		System.out.println(articoloList);
+		result = articoloDAO.visualizzaArticoliJSON();
+		System.out.println(result);
 		return SUCCESS;
 	}
 
